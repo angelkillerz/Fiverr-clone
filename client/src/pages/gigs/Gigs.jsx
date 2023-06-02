98c3c4afa6c375dbe7e6ba5b0df8f4814896f732
Angelkillerz <werrty65@gmail.com>
2023-06-03 04:44:10 +0700

    Update gigs

--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -6,12 +6,19 @@ function Gigs() {
   return (
     <div className='gigs'>
       <div className="container">
-        <span className='breadcrumb'></span>
+        <span className='breadcrumb'>
+          
+        </span>
         <h1>AI Artists</h1>
         <p>Explore the boundaries of art and technology with Fiverr's AI artists</p>
 
         <div className="menu">
-          
+          <div className="left">
+
+          </div>
+          <div className="right">
+
+          </div>
         </div>
       </div>
     </div>
